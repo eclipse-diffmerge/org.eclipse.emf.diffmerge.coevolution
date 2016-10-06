9b138fd71179e76ff3a681fdd5fad8b65370e8fa
Olivier Constant <olivier.constant@thalesgroup.com>
2016-10-06 10:46:25 +0200

    Trivial formatting update
    Change-Id: I460bde4a1168a878e9bebb4d3f15477d468acea8
    Signed-off-by: Olivier Constant <olivier.constant@thalesgroup.com>

--- integrations/transposer/plugins/org.eclipse.emf.diffmerge.bridge.integration.transposer/src/org/eclipse/emf/diffmerge/bridge/integration/transposer/TransposerBridgeOperation.java
+++ integrations/transposer/plugins/org.eclipse.emf.diffmerge.bridge.integration.transposer/src/org/eclipse/emf/diffmerge/bridge/integration/transposer/TransposerBridgeOperation.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  * 
- * Copyright (c) 2015 Thales Global Services S.A.S.
+ * Copyright (c) 2016 Thales Global Services S.A.S.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,6 +39,7 @@ public class TransposerBridgeOperation extends AbstractBridgeOperation {
   /** The optional Transposer configuration */
   private final TransposerConfiguration _configuration;
   
+  
   /**
    * Constructor
    * @param sourceDataSet_p the non-null set of source objects
@@ -69,7 +70,8 @@ public class TransposerBridgeOperation extends AbstractBridgeOperation {
       Collection<Object> sourceElements = getSourceDataSet();
       // Execution setup
       IContext context = transposer.getRulesHandler().getContext();
-      TransposerBridgeContext contextWrapper = new TransposerBridgeContext(context, getBridgeExecution());
+      TransposerBridgeContext contextWrapper =
+          new TransposerBridgeContext(context, getBridgeExecution());
       getBridgeExecution().setup(getTargetDataSet(), contextWrapper);
       // Context initialization
       initializeContext(contextWrapper);
